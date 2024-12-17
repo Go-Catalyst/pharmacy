3156a2ac232eff66b73d43b65ff09f38d0a93f62
Taha <TahaM8000@gmail.com>
2024-12-18 00:47:23 +0330

    add pgdb config to project

--- config/config.go
+++ config/config.go
@@ -1,27 +1,48 @@
 package config
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Configuration struct {
-    Port string
+	Port       string
+	DB         string
+	DBHOST     string
+	DBUSER     string
+	DBPASSWORD string
+	DBPORT     string
+	DBNAME     string
 }
 
 var Config Configuration
 
 func LoadConfig() {
-    Config = Configuration{
-        Port: getEnv("PORT", "8080"),
-    }
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	Config = Configuration{
+		Port:       getEnv("PORT", "8080"),
+		DB:         getEnv("DB", "pg"),
+		DBHOST:     getEnv("DBHOST", ""),
+		DBUSER:     getEnv("DBUSER", ""),
+		DBPASSWORD: getEnv("DBPASSWORD", ""),
+		DBPORT:     getEnv("DBPORT", ""),
+		DBNAME:     getEnv("DBNAME", ""),
+	}
+
 }
 
 func getEnv(key, defaultValue string) string {
-    value, exists := os.LookupEnv(key)
-    if !exists {
-        log.Printf("Environment variable %s not set, using default value %s", key, defaultValue)
-        return defaultValue
-    }
-    return value
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		log.Printf("Environment variable %s not set, using default value %s", key, defaultValue)
+		return defaultValue
+	}
+	return value
 }
