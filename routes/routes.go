bf04aed5102dea2c9b7fccd6b5869d3997bb28d0
myavarih <78411409+myavarih@users.noreply.github.com>
2024-12-20 12:45:24 +0330

    Task Done!

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,15 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	handlers2 "pharmacy/handlers"
+	"pharmacy/handlers"
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	r.GET("/users", handlers2.GetUsers)
-	r.GET("/users/:id", handlers2.GetUser)
-	r.POST("/users", handlers2.CreateUser)
-	r.PUT("/users/:id", handlers2.UpdateUser)
-	r.DELETE("/users/:id", handlers2.DeleteUser)
+	r.GET("/users", handlers.GetUsers)
+	r.GET("/users/:id", handlers.GetUser)
+	r.POST("/users", handlers.CreateUser)
+	r.PUT("/users/:id", handlers.UpdateUser)
+	r.DELETE("/users/:id", handlers.DeleteUser)
 
-	r.POST("/categories", handlers2.CreateCategory)
+	r.POST("/categories", handlers.CreateCategory)
 }
