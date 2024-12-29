c47b87837bc437e598b644d5bd2504163bccbe87
mobinafarzaneh <149579004+mobinafarzaneh@users.noreply.github.com>
2024-12-29 23:50:36 +0330

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,20 +2,20 @@ package routes
 
 import (
     "github.com/gin-gonic/gin"
-    "pharmacy/internal/users/handlers"
-    "pharmacy/internal/categories/handlers"
+    "pharmacy/internal/users"
+    "pharmacy/internal/categories"
 
 )
 
 func RegisterRoutes(r *gin.Engine) {
-    r.GET("/users", handlers.GetUsers)
-    r.GET("/users/:id", handlers.GetUser)
-    r.POST("/users", handlers.CreateUser)
-    r.PUT("/users/:id", handlers.UpdateUser)
-    r.DELETE("/users/:id", handlers.DeleteUser)
-    r.GET("/categories", handlers.GetCategories)
-    r.GET("/categories/:id", handlers.GetCategory)
-    r.POST("/categories", handlers.CreateCategory)
-    r.PUT("/categories/:id", handlers.UpdateCategory)
-    r.DELETE("/categories/:id", handlers.DeleteCategory)
+    r.GET("/users", users.handlers.GetUsers)
+    r.GET("/users/:id", users.handlers.GetUser)
+    r.POST("/users", users.handlers.CreateUser)
+    r.PUT("/users/:id", users.handlers.UpdateUser)
+    r.DELETE("/users/:id", users.handlers.DeleteUser)
+    r.GET("/categories", categories.handlers.GetCategories)
+    r.GET("/categories/:id", categories.handlers.GetCategory)
+    r.POST("/categories", categories.handlers.CreateCategory)
+    r.PUT("/categories/:id", categories.handlers.UpdateCategory)
+    r.DELETE("/categories/:id", categories.handlers.DeleteCategory)
 }
