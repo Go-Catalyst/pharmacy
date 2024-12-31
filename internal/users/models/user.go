64e01fd62a52757e18d09b8174291104b3efde06
MORADYT <amirmohammadmf619@gmail.com>
2024-12-31 18:06:16 +0330

    user model updated

--- internal/users/models/user.go
+++ internal/users/models/user.go
@@ -1,7 +1,19 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type User struct {
-    ID    uint   `json:"id" gorm:"primary_key"`
-    Name  string `json:"name"`
-    Email string `json:"email"`
+	gorm.Model
+	Email       string     `gorm:"unique;not null" json:"email"`
+	Username    string     `gorm:"unique;not null" json:"username"`
+	Password    string     `gorm:"not null" json:"password"`
+	FirstName   string     `gorm:"not null" json:"firstName"`
+	LastName    string     `gorm:"not null" json:"lastName"`
+	PhoneNumber string     `gorm:"unique;not null" json:"phoneNumber"`
+	Role        string     `gorm:"not null" json:"role"`
+	IsActive    bool       `gorm:"default:true" json:"isActive"`
+	LastLoginAt *time.Time `gorm:"default:null" json:"lastLoginAt"`
 }
